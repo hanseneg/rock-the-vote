1f6b9430f20e0bad24138f1fd949954cfbdd064a
Ethan Hansen <hanseneg@hotmail.com>
2021-03-26 22:41:50 -0600

    commit8

---
+++ client/src/components/CommentForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CommentForm(){
+    
+    return (
+        <form>
+            <input />
+            <button>Submit Comment</button>
+        </form>
+    )
+}
