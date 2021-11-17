560ce0a866650e720bcd7840fea11ffbfe268f44
Ethan Hansen <hanseneg@hotmail.com>
2021-11-16 17:58:40 -0700

    commit21

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 const expressJwt = require('express-jwt')
 
+const PORT = process.env.PORT || 8080
+
 app.use(express.json())
 app.use(morgan('dev'))
 
-mongoose.connect(
+mongoose.connect(process.env.MONGODB_URI ||
   'mongodb://localhost:27017/rock-the-vote',
   {
     useNewUrlParser: true,
@@ -33,6 +35,10 @@ app.use((err, req, res, next) => {
   }
   return res.send({errMsg: err.message})
 })
+
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+}
 
 app.listen(9000, () => {
   console.log(`Server is running on local port 9000`)
