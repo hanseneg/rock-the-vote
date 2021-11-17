6dca960fe2a9fab4f7ae6d4639368aa01aec8242
Ethan Hansen <hanseneg@hotmail.com>
2021-11-16 18:12:18 -0700

    commit22

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const authRouter = express.Router()
-const User = require('../models/user.js')
+//const User = require('../models/user.js')
+const User = require('../models.User.js')
 const jwt = require('jsonwebtoken')
 
 //signup
